50d5f11ffa25d63b1e957042e0f38c443344ef78
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2022-11-18 18:23:08 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'TypingGame';
+export class AppComponent implements OnInit {
+
+  randomText: string = '';
+  enteredText: string = '';
+  randomTextSpacesCount: number = 0;
+  enteredTextSpacesCount: number = 0;
+
+  ngOnInit(): void {
+    this.randomText = lorem.sentence();
+    this.randomTextSpacesCount = this.randomText.split(' ').length-1;
+    console.log("oninit", this.randomTextSpacesCount);
+  }
+
+  onInput = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    this.enteredText = target.value;
+    this.enteredTextSpacesCount = this.enteredText.split(' ').length-1;
+  };
+
+  compare = (randomLetter: string, enteredLetter: string): string => {
+
+    if (!enteredLetter) {
+      return "pending";
+    }
+
+    return randomLetter === enteredLetter ? "correct" : "incorrect";
+  };
 }
